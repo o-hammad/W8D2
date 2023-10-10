389ca9bbd305157adc7b43da7b7a590668af2831
Omar <ohammad@gwu.edu>
2023-10-10 13:40:38 -0400

    starting iterations

--- phase-3-iteration.js
+++ phase-3-iteration.js
@@ -1,1 +1,23 @@
 // Your code here
+
+Array.prototype.bubbleSort = function () {
+    let sorted = false;
+
+    while (!sorted){
+        sorted = true;
+
+        for(let i = 0; i < this.length - 1; i++) {
+            if (this[i] > this[i + 1]) {
+                temp = this[i];
+                this[i] = this[i + 1];
+                this[i + 1] = temp;
+                sorted = false;
+            }
+        }
+    }
+    return this;
+}
+
+let arr1 = [3, 9, 1, 7, 5];
+
+console.log(arr1.bubbleSort());
